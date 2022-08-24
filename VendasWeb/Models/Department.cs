1bcb7f0c67f496bc6c1a368473cd1e10a4dac799
Paulo Henrique Batista de Jesus <paulohenriquephb29@gmail.com>
2022-08-24 00:55:03 -0300

    Cadastro de vendedores

---
+++ VendasWeb/Models/Department.cs
@@ -0,0 +1,39 @@
+﻿using Castle.Components.DictionaryAdapter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+       
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+    }
+}
